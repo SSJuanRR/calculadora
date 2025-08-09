4993173d64e1f3a9b195b9553cb245759cedb0b6
SSJuanRR <5juansebas5@gmail.com>
2025-08-08 22:11:58 -0500

    Actualizado

---
+++ menu.py
@@ -0,0 +1,18 @@
+
+
+def menu():
+    print("\n--- Calculadora ---")
+    print("1. Sumar")
+    print("2. Restar")
+print("3. Multiplicar")
+print("4. Dividir") 
+   
+while True:
+    menu()
+    opcion = input("Elige una opción: ")
+
+  
+    num1 = float(input("Primer número: "))
+    num2 = float(input("Segundo número: "))
+
+  
