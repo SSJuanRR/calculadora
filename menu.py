b4068ed8f692106259d36e23bfae6cdc7d2ef9af
michela1025 <michellquintero2020@gmail.com>
2025-08-08 23:06:20 -0500

    listo

--- menu.py
+++ menu.py
@@ -1,18 +1,12 @@
 
-
-def menu():
-    print("\n--- Calculadora ---")
-    print("1. Sumar")
-    print("2. Restar")
-print("3. Multiplicar")
-print("4. Dividir") 
-   
-while True:
-    menu()
-    opcion = input("Elige una opción: ")
-
-  
-    num1 = float(input("Primer número: "))
-    num2 = float(input("Segundo número: "))
-
+ if opcion == "1":
+        print("Resultado:", sumar(num1, num2))
+    elif opcion == "2":
+        print("Resultado:", restar(num1, num2))
+    elif opcion == "3":
+        print("Resultado:", multiplicar(num1, num2))
+    elif opcion == "4":
+        print("Resultado:", dividir(num1, num2))
+    else:
+        print("Opción no válida")
   
